3997ae405bd0bc3a4593c47b98a106b9d29df12e
ahmedtaju <ahmmedahmd02@gmail.com>
2025-02-09 04:53:33 +0300

    challenge three making ui.

--- lib/main.dart
+++ lib/main.dart
@@ -19,96 +19,75 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
           body: Column(
         children: [
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(1);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.red,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(1);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.red,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(2);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.yellow,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(2);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.yellow,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(3);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.green,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(3);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.purple,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(4);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(4);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.teal,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(5);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.purple,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(5);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.pink,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(6);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.pink,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(6);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.blue,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                playSound(7);
-              },
+          Expanded(
+            child: Container(
+              color: Colors.teal,
+              child: GestureDetector(
+                onTap: () {
+                  playSound(7);
+                },
+              ),
             ),
-            width: 100,
-            height: 30,
-            color: Colors.orange,
           ),
         ],
       )),
