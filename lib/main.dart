9ee299bd15b6076b929d1696afcc2e8f864c066c
ahmedtaju <ahmmedahmd02@gmail.com>
2025-02-09 04:21:07 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,27 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: GestureDetector(
+            child: Text('PLAY'),
+            onTap: () {
+              final player = AudioPlayer();
+              player.play(AssetSource('note2.wav'));
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
