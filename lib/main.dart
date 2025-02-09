eb8390ad0a9cc3e8ca51f800bc862e86a6cb7b77
ahmedtaju <ahmmedahmd02@gmail.com>
2025-02-09 04:30:58 +0300

    challenge one

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,108 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            child: Text('PLAY'),
-            onTap: () {
-              final player = AudioPlayer();
-              player.play(AssetSource('note2.wav'));
-            },
+          body: Column(
+        children: [
+          SizedBox(
+            height: 20,
           ),
-        ),
-      ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note1.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.red,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note2.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.yellow,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note3.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.purple,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note4.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.teal,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note5.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.pink,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note6.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.blue,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note7.wav'));
+              },
+            ),
+            width: 100,
+            height: 30,
+            color: Colors.orange,
+          ),
+        ],
+      )),
     );
   }
 }
